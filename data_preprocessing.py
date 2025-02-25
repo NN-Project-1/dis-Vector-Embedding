873c3e48c6798b8e3e24165003805a36ab5e94d6
NN-Project-1 <162957988+NN-Project-1@users.noreply.github.com>
2025-02-25 09:22:30 +0530

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,115 +5,114 @@ import soundfile as sf
 from numpy.random import RandomState
 from utils import *
 
-def make_spect_f0(config):
-    fs = 16000
-    data_dir = config.data_dir
-    feat_dir = config.feat_dir
-    wav_dir = os.path.join(feat_dir, config.wav_dir)
-    spmel_dir = os.path.join(feat_dir, config.spmel_dir)
-    f0_dir = os.path.join(feat_dir, config.f0_dir)
-    spk_meta = pickle.load(open('spk_meta.pkl', "rb"))
+def generate_spectrogram_and_f0(config):
+    sample_rate = 16000
+    data_path = config.data_dir
+    feature_path = config.feat_dir
+    wav_output_path = os.path.join(feature_path, config.wav_dir)
+    spmel_output_path = os.path.join(feature_path, config.spmel_dir)
+    f0_output_path = os.path.join(feature_path, config.f0_dir)
+    speaker_metadata = pickle.load(open('spk_meta.pkl', "rb"))
 
-    dir_name, spk_dir_list, _ = next(os.walk(data_dir))
-    state_count = 1
+    base_dir, speaker_list, _ = next(os.walk(data_path))
+    random_seed = 1
 
-    for spk_dir in sorted(spk_dir_list):
-        if spk_dir not in spk_meta:
-            print(f'Warning: {spk_dir} not in speaker metadata; skip generating features')
+    for speaker_id in sorted(speaker_list):
+        if speaker_id not in speaker_metadata:
+            print(f'Warning: {speaker_id} not found in speaker metadata; skipping feature generation.')
             continue
-        print(f'Generating features for speaker {spk_dir}')
+        print(f'Processing features for speaker {speaker_id}')
         
-        for fea_dir in [wav_dir, spmel_dir, f0_dir]:
-            if not os.path.exists(os.path.join(fea_dir, spk_dir)):
-                os.makedirs(os.path.join(fea_dir, spk_dir))
+        for output_folder in [wav_output_path, spmel_output_path, f0_output_path]:
+            os.makedirs(os.path.join(output_folder, speaker_id), exist_ok=True)
 
-        _,_, file_list = next(os.walk(os.path.join(dir_name,spk_dir)))
+        _, _, audio_files = next(os.walk(os.path.join(base_dir, speaker_id)))
 
-        if spk_meta[spk_dir][1] == 'M':
-            lo, hi = 50, 250
-        elif spk_meta[spk_dir][1] == 'F':
-            lo, hi = 100, 600
+        if speaker_metadata[speaker_id][1] == 'M':
+            pitch_min, pitch_max = 50, 250
+        elif speaker_metadata[speaker_id][1] == 'F':
+            pitch_min, pitch_max = 100, 600
         else:
             continue
 
-        prng = RandomState(state_count) 
-        wavs, f0s, sps, aps = [], [], [], []
-        for filename in sorted(file_list):
-            # read audios
-            x, _ = sf.read(os.path.join(dir_name,spk_dir,filename))
-            if x.shape[0] % 256 == 0:
-                x = np.concatenate((x, np.array([1e-06])), axis=0)
-            wav = filter_wav(x, prng)
+        rng = RandomState(random_seed) 
+        audio_waveforms, f0_values, spectral_envelopes, aperiodicities = [], [], [], []
+        for audio_file in sorted(audio_files):
+            signal, _ = sf.read(os.path.join(base_dir, speaker_id, audio_file))
+            if signal.shape[0] % 256 == 0:
+                signal = np.concatenate((signal, np.array([1e-06])), axis=0)
+            filtered_signal = filter_wav(signal, rng)
 
-            # get WORLD analyzer parameters
-            f0, sp, ap = get_world_params(wav, fs)
-            wavs.append(wav)
-            f0s.append(f0)
-            sps.append(sp)
-            aps.append(ap)
+            # Extract WORLD analyzer parameters
+            f0, spectral_envelope, aperiodicity = get_world_params(filtered_signal, sample_rate)
+            audio_waveforms.append(filtered_signal)
+            f0_values.append(f0)
+            spectral_envelopes.append(spectral_envelope)
+            aperiodicities.append(aperiodicity)
 
-        # smooth pitch to synthesize monotonic speech
-        f0s = average_f0s(f0s, mode='global')
+        # Smooth pitch for monotonic speech synthesis
+        f0_values = average_f0s(f0_values, mode='global')
 
-        for idx, (filename, wav, f0, sp, ap) in enumerate(zip(file_list, wavs, f0s, sps, aps)):
+        for idx, (audio_file, waveform, f0, sp_env, ap_env) in enumerate(zip(audio_files, audio_waveforms, f0_values, spectral_envelopes, aperiodicities)):
 
-            wav_mono = get_monotonic_wav(wav, f0, sp, ap, fs)
-            spmel = get_spmel(wav)
-            f0_rapt, f0_norm = extract_f0(wav, fs, lo, hi)
-            assert len(spmel) == len(f0_rapt)
+            monotonic_waveform = get_monotonic_wav(waveform, f0, sp_env, ap_env, sample_rate)
+            spectrogram_mel = get_spmel(waveform)
+            f0_extracted, f0_normalized = extract_f0(waveform, sample_rate, pitch_min, pitch_max)
+            assert len(spectrogram_mel) == len(f0_extracted)
 
-            # segment feature into trunks with the same length during training
-            start_idx = 0
-            trunk_len = 49151
-            while start_idx*trunk_len < len(wav_mono):
-                wav_mono_trunk = wav_mono[start_idx*trunk_len:(start_idx+1)*trunk_len]
-                if len(wav_mono_trunk) < trunk_len:
-                    wav_mono_trunk = np.pad(wav_mono_trunk, (0, trunk_len-len(wav_mono_trunk)))
-                np.save(os.path.join(wav_dir, spk_dir, os.path.splitext(filename)[0]+'_'+str(start_idx)),
-                        wav_mono_trunk.astype(np.float32), allow_pickle=False)
-                start_idx += 1
-            feas = [spmel, f0_norm]
-            fea_dirs = [spmel_dir, f0_dir]
-            for fea, fea_dir in zip(feas, fea_dirs):
-                start_idx = 0
-                trunk_len = 192
-                while start_idx*trunk_len < len(fea):
-                    fea_trunk = fea[start_idx*trunk_len:(start_idx+1)*trunk_len]
-                    if len(fea_trunk) < trunk_len:
-                        if fea_trunk.ndim==2:
-                            fea_trunk = np.pad(fea_trunk, ((0, trunk_len-len(fea_trunk)), (0, 0)))
+            # Segment features into fixed-length chunks for training
+            segment_start = 0
+            segment_size = 49151
+            while segment_start * segment_size < len(monotonic_waveform):
+                waveform_segment = monotonic_waveform[segment_start * segment_size:(segment_start + 1) * segment_size]
+                if len(waveform_segment) < segment_size:
+                    waveform_segment = np.pad(waveform_segment, (0, segment_size - len(waveform_segment)))
+                np.save(os.path.join(wav_output_path, speaker_id, os.path.splitext(audio_file)[0] + f'_{segment_start}'),
+                        waveform_segment.astype(np.float32), allow_pickle=False)
+                segment_start += 1
+            
+            features = [spectrogram_mel, f0_normalized]
+            output_paths = [spmel_output_path, f0_output_path]
+            for feature, output_path in zip(features, output_paths):
+                segment_start = 0
+                segment_size = 192
+                while segment_start * segment_size < len(feature):
+                    feature_segment = feature[segment_start * segment_size:(segment_start + 1) * segment_size]
+                    if len(feature_segment) < segment_size:
+                        if feature_segment.ndim == 2:
+                            feature_segment = np.pad(feature_segment, ((0, segment_size - len(feature_segment)), (0, 0)))
                         else:
-                            fea_trunk = np.pad(fea_trunk, ((0, trunk_len-len(fea_trunk)), ))
-                    np.save(os.path.join(fea_dir, spk_dir, os.path.splitext(filename)[0]+'_'+str(start_idx)),
-                            fea_trunk.astype(np.float32), allow_pickle=False)
-                    start_idx += 1
+                            feature_segment = np.pad(feature_segment, ((0, segment_size - len(feature_segment)), ))
+                    np.save(os.path.join(output_path, speaker_id, os.path.splitext(audio_file)[0] + f'_{segment_start}'),
+                            feature_segment.astype(np.float32), allow_pickle=False)
+                    segment_start += 1
 
 
-def make_metadata(config):
-    feat_dir = config.feat_dir
-    wav_dir = os.path.join(feat_dir, config.wav_dir) 
-    dir_name, spk_dir_list, _ = next(os.walk(wav_dir))
-    spk_meta = pickle.load(open('spk_meta.pkl', "rb"))
-    dataset = []
+def generate_metadata(config):
+    feature_path = config.feat_dir
+    wav_output_path = os.path.join(feature_path, config.wav_dir) 
+    base_dir, speaker_list, _ = next(os.walk(wav_output_path))
+    speaker_metadata = pickle.load(open('spk_meta.pkl', "rb"))
+    dataset_entries = []
 
-    for spk_dir in sorted(spk_dir_list):
-        spk_id, _ = spk_meta[spk_dir]
-        spk_emb = np.zeros((config.dim_spk_emb,), dtype=np.float32)
-        spk_emb[int(spk_id)] = 1.0
+    for speaker_id in sorted(speaker_list):
+        speaker_index, _ = speaker_metadata[speaker_id]
+        speaker_embedding = np.zeros((config.dim_spk_emb,), dtype=np.float32)
+        speaker_embedding[int(speaker_index)] = 1.0
 
-        utterances = []
-        _, _, file_list = next(os.walk(os.path.join(dir_name, spk_dir)))
+        utterance_list = []
+        _, _, file_list = next(os.walk(os.path.join(base_dir, speaker_id)))
         file_list = sorted(file_list)
-        for filename in file_list:
-            utterances.append(os.path.join(spk_dir,filename))
-        for utterance in utterances:
-            dataset.append((spk_dir, spk_emb, utterance))
+        for audio_file in file_list:
+            utterance_list.append(os.path.join(speaker_id, audio_file))
+        for utterance in utterance_list:
+            dataset_entries.append((speaker_id, speaker_embedding, utterance))
 
-    with open(os.path.join(feat_dir, 'dataset.pkl'), 'wb') as handle:
-        pickle.dump(dataset, handle)
+    with open(os.path.join(feature_path, 'dataset.pkl'), 'wb') as metadata_file:
+        pickle.dump(dataset_entries, metadata_file)
 
-def preprocess_data(config):
-    print('Start preprocessing...')
-    make_spect_f0(config)
-    make_metadata(config)
-    print('Done')
+def preprocess_audio_data(config):
+    print('Starting audio data preprocessing...')
+    generate_spectrogram_and_f0(config)
+    generate_metadata(config)
+    print('Preprocessing completed.')
